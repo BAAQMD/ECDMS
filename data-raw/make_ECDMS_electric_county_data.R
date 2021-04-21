899691d6b88963e831172c144b0f59667b637fbc
David Holstius <david.holstius@gmail.com>
2021-04-21 14:39:27 -0700

    add validation step in make_ECDMS_electric_county_data()

--- data-raw/make_ECDMS_electric_county_data.R
+++ data-raw/make_ECDMS_electric_county_data.R
@@ -21,10 +21,31 @@ make_ECDMS_electric_county_data <- function (
     gather_years("tput_qty") %>%
     mutate_at(
       vars(year),
-      ~ CY(elide_year(.)))
+      ~ CY(elide_year(.))) %>%
+    vartools::drop_vars(
+      `Total Usage`)
+
+  validated <-
+    reshaped %>%
+    spread(
+      sector,
+      tput_qty) %>%
+    mutate(
+      Ratio = (`Non-Residential` + `Residential`) / Total,
+      Diff = `Non-Residential` + `Residential` - Total) %>%
+    ensurer::ensure(
+      all(abs(.$Diff) < 0.1, na.rm = TRUE),
+      all(abs(log(.$Ratio)) < 0.1, na.rm = TRUE)) %>%
+    drop_vars(
+      Total, Ratio, Diff) %>%
+    gather(
+      sector,
+      tput_qty,
+      `Non-Residential`,
+      `Residential`)
 
   tidied <-
-    reshaped %>%
+    validated %>%
     filter(
       sector != "Total") %>%
     mutate(
